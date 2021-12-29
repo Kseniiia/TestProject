3630e94fab73f0e296248ec724083f229b053802
Kseniya Malchykava <kseniya_malchykava@epam.com>
2021-12-29 21:21:39 +0300

    add script

---
+++ TestProject2/script.ps1
@@ -0,0 +1,15 @@
+# Navigate to tests folder
+#cd C:\TestProject\TestProject1
+pwd
+
+# Build
+Write-Output "Run tests"
+
+& 'C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\msbuild' TestProject2\TestProject2.sln "/t:Clean,Build" "/p:Configuration=Release"
+
+# Run tests
+$launch_name ='TEST'
+$env:RP_LAUNCH_NAME = $launch_name
+$runner_path ='C:\TestProject\TestProject2\TestProject2\nunit\nunit3-console.exe'
+$testlibpath = '.\TestProject2\bin\Debug\netcoreapp3.1\TestProject2.dll'
+& $runner_path $testlibpath --where="(test =~ /TestProject2/) AND test !~ /Unauthorized/
