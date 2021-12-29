05de5ece28c0ded10762d7acc8e73433d487c4df
Kseniya Malchykava <kseniya_malchykava@epam.com>
2021-12-29 21:31:29 +0300

    fix script

--- TestProject2/script.ps1
+++ TestProject2/script.ps1
@@ -7,6 +7,10 @@ Write-Output "Run tests"
 
 & 'C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\msbuild' TestProject2\TestProject2.sln "/t:Clean,Build" "/p:Configuration=Release"
 
+# Enable Report Portal Nunit plugin
+echo ../../../TestProject2/bin/Debug/netcoreapp3.1/ReportPortal.NUnitExtension.dll > .\nunit\ReportPortal.addins
+echo ../../../TestProject2/bin/Debug/netcoreapp3.1/TestProject2.dll > .\nunit\ReportPortalCustomization.addins
+
 # Run tests
 $launch_name ='TEST'
 $env:RP_LAUNCH_NAME = $launch_name
