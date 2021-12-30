53dfa26739f70fe9de93b154828c00d0e934fdf1
Kseniya Malchykava <kseniya_malchykava@epam.com>
2021-12-30 12:33:45 +0300

    fix tests

--- TestProject2/TestProject2/UnitTest1.cs
+++ TestProject2/TestProject2/UnitTest1.cs
@@ -13,6 +13,8 @@ namespace TestProject2
         [SetUp]
         public void Setup()
         {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("no-sandbox");
             driver.Navigate().GoToUrl(url);
         }
 
