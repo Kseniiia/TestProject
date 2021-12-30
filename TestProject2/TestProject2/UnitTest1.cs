a474f7d3d0393c36f25d2ea7a8567bb1053d8b3d
Kseniya Malchykava <kseniya_malchykava@epam.com>
2021-12-30 12:57:35 +0300

    fix tests

--- TestProject2/TestProject2/UnitTest1.cs
+++ TestProject2/TestProject2/UnitTest1.cs
@@ -1,27 +1,18 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System;
 
 namespace TestProject2
 {
     public class Tests
     {
-        string chromeDriverDirectory = "C:\\TestProject\\TestProject2\\TestProject2\\bin\\Release\\netcoreapp3.1";
         IWebElement element;
-        ChromeOptions options = new ChromeOptions();
-       // ChromeDriver driver;
-        //WebDriver driver = new ChromeDriver();
-
+        WebDriver driver = new ChromeDriver();
         string url = "https://github.com/nunit/nunit-console/releases/tag/3.13";
 
         [SetUp]
         public void Setup()
         {
-            var driver = new ChromeDriver(chromeDriverDirectory, options, TimeSpan.FromMinutes(3));
-            driver.Manage().Timeouts().PageLoad.Add(System.TimeSpan.FromSeconds(30));
-            options.AddArgument("no-sandbox");
-          
             driver.Navigate().GoToUrl(url);
         }
 
@@ -40,7 +31,7 @@ namespace TestProject2
         [Test]
         public void Test3()
         {
-           // element = driver.FindElement(By.XPath("//*[@id='repo - content - pjax - container']/div/div/div[1]/div[2]/div[1]/h1"));
+            element = driver.FindElement(By.XPath("//*[@id='repo - content - pjax - container']/div/div/div[1]/div[2]/div[1]/h1"));
             Assert.IsNotNull(element);
         }
     }
